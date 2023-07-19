bb74742c9ba3c91b5b15e210ecd3691f224e8887
petrbinek <binekpeter98@gmail.com>
2023-07-19 13:58:29 +0300

    Алгоритм

---
+++ Алгоритмы/Program.cs
@@ -0,0 +1,53 @@
+﻿class User
+{
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public bool IsPremium { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создание списка пользователей с примером данных
+        List<User> users = new List<User>
+        {
+            new User { Login = "user1", Name = "Гарри", IsPremium = true },
+            new User { Login = "user2", Name = "Рон", IsPremium = false },
+            new User { Login = "user3", Name = "Гермиона", IsPremium = true }
+        };
+
+        // Перебор пользователей в списке
+        foreach (var user in users)
+        {
+            // Вызов метода для приветствия пользователя
+            GreetUser(user);
+
+            // Проверка наличия премиум-подписки у пользователя
+            if (!user.IsPremium)
+            {
+                // Вызов метода для показа рекламы
+                ShowAds();
+            }
+        }
+    }
+
+    // Метод для приветствия пользователя по имени
+    static void GreetUser(User user)
+    {
+        Console.WriteLine($"Привет, {user.Name}!");
+    }
+
+    // Метод для показа рекламы
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        Thread.Sleep(3000);
+    }
+}
